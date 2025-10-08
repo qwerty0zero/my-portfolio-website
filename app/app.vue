61bfbc7abb98910b07e1e0920c93acba41f953a3
iBiltaz <56063464+qwerty0zero@users.noreply.github.com>
2025-10-09 00:41:23 +0200

    Prepare SSG build for Vercel

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     <NuxtPage />
+    <SpeedInsights/>
 <!--    <Preloader/>-->
   </div>
 </template>
 
 <script setup>
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+
 import Preloader from "~/components/Preloader.vue";
 </script>
 
