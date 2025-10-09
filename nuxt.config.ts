f0e75e7b30f528eee20656490b971d8d23cf399d
iBiltaz <56063464+qwerty0zero@users.noreply.github.com>
2025-10-09 03:58:29 +0200

    Fix baseURL and router base for GitHub Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,12 @@
-
 import projects from './app/assets/data/projects.json'
 
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
-    ssr: false,      // отключаем SSR
-    target: 'static', // SSG
+    ssr: false,          // отключаем SSR
+    target: 'static',     // SSG
+    router: {
+        base: '/my-portfolio-website/' // имя репозитория для GitHub Pages
+    },
     nitro: {
         preset: 'vercel_static',
         prerender: {
@@ -14,7 +16,7 @@ export default defineNuxtConfig({
             ]
         }
     },
-    app:{
+    app: {
         head: {
             title: 'Ihor Kuhel',
             htmlAttrs: {
@@ -23,12 +25,13 @@ export default defineNuxtConfig({
             meta: [{ name: 'theme-color', content: '#d52627' }],
         },
     },
-
     devtools: { enabled: true },
     modules: ['@nuxt/eslint', '@nuxt/fonts'],
     css: [
         '~/assets/styles/variables.css',
         '~/assets/styles/style.css'
-    ]
-    // plugins здесь НЕ нужно
+    ],
+    build: {
+        publicPath: '/my-portfolio-website/_nuxt/' // чтобы JS/CSS корректно подгружались
+    }
 })
